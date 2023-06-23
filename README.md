f6769542d04ff35073ce73b2f13e00456ac19884
parthan_m <parthan.manisekaran@rwth-aachen.de>
2023-06-23 22:47:25 +0200

    updated README and the docker path, trying to run TFServing GPU

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # vehicle-cloud-inference
 Part of the ACDC Research Project
+
+
+## MQTT Inference method
+
+WRITE DOCUMENTATION HERE
+
+## TF-Serving Inference method
+
+In order to run the tf-serving
+
+First let's run the Tensorflow Serving Docker Container (CPU) by running
+
+`docker run -p 8501:8501 --mount type=bind,source=/home/typlosion/work/ACDC/vehicle-cloud-inference/tf_serving/model/1/,target=/models/mobilenet/1/ -e MODEL_NAME=mobilenet -t tensorflow/serving`
+
+
+OR Using GPU (CHECK NOTE BEFORE RUNNING) by runnning
+
+`docker run --gpus all -p 8501:8501 --mount type=bind,source=/home/typlosion/work/ACDC/vehicle-cloud-inference/tf_serving/model/1/,target=/models/mobilenet/1/ -e MODEL_NAME=mobilenet -t tensorflow/serving:latest-gpu`
+
+
+After that, just run the segmentation_mobilenet_serving.py to get the inference
+
+NOTE
+1. In order to run the tensorflow serving docker, you must pull them first
+
+FOR CPU
+`docker pull tensorflow/serving` 
+
+FOR GPU 
+
+You've gotta to install the nvidia driver and the nvidia container toolkit before using this
+
+check out the links below
+[Nvidia Driver installation](https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-ubuntu-22-04)
+
+[Nvidia Container toolkit installation for Docker](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#docker)
+
+Once set up,
+
+`docker pull tensorflow/serving:latest-gpu` 
--- tf_serving/custom_signature_segmentation.py
+++ tf_serving/custom_signature_segmentation.py
@@ -7,9 +7,8 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from img_utils import resize_image
 import cv2
-import xml.etree.ElementTree as ET
 
-
+model = tf.keras.load_model('model/1')
 
 def segmentation_map_to_rgb(segmentation_map,color_palette):
     """
@@ -69,63 +68,21 @@ def parse_convert_xml(conversion_file_path):
 
 
 
-#model = tf.keras.models.load_model('model')
-#path_to_xml = 'cityscapes.xml'
+def preprocess(image):
+    path_to_xml = 'convert.xml'
+    color_palette, class_names, color_to_label = parse_convert_xml(path_to_xml)
 
-path_to_xml = 'convert.xml'
-color_palette, class_names, color_to_label = parse_convert_xml(path_to_xml)
+    width = 2048
+    height = 1024
 
-width = 2048
-height = 1024
-
-input_img = cv2.imread('image.png')
-input_img = resize_image(input_img,[height,width])
+    input_img = cv2.imread('image.png')
+    input_img = resize_image(input_img,[height,width])
 
 
 
-batched_img = tf.expand_dims(input_img, axis=0)
-batched_img = tf.cast(batched_img, tf.uint8)
-print(f"Batched image shape: {batched_img.shape}")
+    batched_img = tf.expand_dims(input_img, axis=0)
+    batched_img = tf.cast(batched_img, tf.uint8)
+    #print(f"Batched image shape: {batched_img.shape}")
 
 
-# model_outputs = model(batched_img)
-# print(f"Model output shape: {model_outputs.shape}")
-# print(f"Predicted class: {postprocess(model_outputs)}")
-
-
-
-### Serving part 
-
-data = json.dumps(
-    {"signature_name": "serving_default", "instances": batched_img.numpy().tolist()}
-)
-
-
-# Docker command for setting up Server :
-'''
-CHECK README
-'''
-url = "http://localhost:8501/v1/models/mobilenet:predict"
-
-
-def predict_rest(json_data, url):
-    json_response = requests.post(url, data=json_data)
-    response = json.loads(json_response.text)
-    #print(response)
-    predictions = np.array(response["predictions"])
-    prediction = tf.squeeze(predictions).numpy()
-    argmax_prediction = np.argmax(prediction, axis=2)
-    prediction = segmentation_map_to_rgb(argmax_prediction,color_palette=color_palette).astype(np.uint8)
-    prediction = cv2.cvtColor(prediction,cv2.COLOR_BGR2RGB)
-
-    return prediction
-
-
-print("Now using the serving \n")
-prediction = predict_rest(data, url)
-
-
-print(f"REST output shape: {prediction.shape}")
-cv2.imshow('prediction',prediction)
-cv2.waitKey(0)
-#print(f"Predicted class: {postprocess(rest_outputs)}")
+def postprocess()
